3cb26ae3f22fca2d41eb1301483144c80e27e8bc
Edex13 <ederson12f@gmail.com>
2021-05-24 21:02:42 -0500

    soluciono bug del buscador

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,37 +2,42 @@ import getData from '../utils/getData';
 import getHash from '../utils/getHash';
 import templateCard from '../template/templateCard';
 import templateCountry from '../template/templateCountry';
+
 const router = async () => {
    let hash = getHash();
    let data = await getData();
-   hash === '/' ? await templateCard(data) : await uniqueHash(hash);
+   hash === '/' ? searh(data) : uniqueHash(hash);
 
    function filterHash(hash) {
-      console.log(hash);
       let filteredData = data.filter((country) => {
-         return country.name.toLocaleLowerCase().indexOf(hash) == 0;
+         return country.name.toLocaleLowerCase().indexOf(hash.toLocaleLowerCase()) == 0;
       });
       templateCard(filteredData);
    }
-   function uniqueHash(hash){
+   function uniqueHash(hash) {
       let filteredData = data.filter((country) => {
          return country.name.toLocaleLowerCase() == hash;
       });
-      if (filteredData == []){
-         console.log("puto")
-         return false 
-      }
+      console.log(data)
       templateCountry(filteredData);
    }
-   const input = document.querySelector('.input');
-   input.addEventListener('input', (e) => {
-      let value = e.srcElement.value
-      if (value  == '') {
-         templateCard(data);
-      } else {
-         filterHash(value);
-      }
-   });
+   function searh(data) {
+      const searh = document.querySelector('#container_search');
+      searh.classList.add('container__search_input')
+      searh.innerHTML = `
+            <input type="text" placeholder="Search for a country..." class="input" />
+            <img src="../src/assets/images/lupa.svg" alt="icon-lupa" class="search__logo" />`;
+      const input = document.querySelector('.input');
+      input.addEventListener('input', (e) => {
+         let value = e.srcElement.value;
+         if (value == '') {
+            templateCard(data);
+         } else {
+            filterHash(value);
+         }
+      });
+      templateCard(data);
+   }
 };
 
 export default router;
--- src/template/templateCountry.js
+++ src/template/templateCountry.js
@@ -1,13 +1,16 @@
 import '../components/countryDetails';
 
 const templateCard = (countrys) => {
+
    const details = document.getElementById('container');
    details.classList.remove('cards');
    details.innerHTML = '';
-   const searh = document.querySelector('.container__search_input');
-   searh.innerHTML = ``;
-   const country = countrys[0];
 
+   const searh = document.querySelector('#container_search');
+   searh.classList.remove('container__search_input');
+   searh.innerHTML = ``
+
+   const country = countrys[0];
    let card = document.createElement('country-details');
    console.log(countrys);
    attributes(card, 'img', country.flag);
@@ -19,7 +22,6 @@ const templateCard = (countrys) => {
    attributes(card, 'capital', country.capital);
    attributes(card, 'top_level_domain', country.topLevelDomain[0]);
    attributes(card, 'currencies', `${country.currencies[0].code} - ${country.currencies[0].name}`);
-   console.log(card);
    details.append(card);
 };
 
